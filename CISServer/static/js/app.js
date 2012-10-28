d4d043d1919a207788752501f423fb7f27764e57
exiledude22 <aurelian.cojocaru89@gmail.com>
2012-10-28 07:14:17 -0700

    - added CIS management interface prototype

---
+++ CISServer/static/js/app.js
@@ -0,0 +1,44 @@
+﻿$(document).ready(function () {
+    var refresh_interval = 1000;
+    $(".hotspot").click(function () {
+        console.log("hotspot clicked");
+        $(this).css("margin-left", "1000px");
+        var element = this;
+        setTimeout(function () {
+            $(element).remove();
+        }, 400);
+    });
+
+    $(".service-container").click(function () {
+
+
+        var hotspot_id = $(this).attr("hotspot-id");
+        var service_id = $(this).attr("service-id");
+        var service_container = $(this);
+        var parent_offset = service_container.offset();
+        service_container.css("top", parent_offset.top);
+        service_container.css("left", parent_offset.left);
+
+        $.each($(".hotspot"), function () {
+            if ($(this).attr("hotspot-id") == hotspot_id) {
+                $.each($(".static-service-container", $(this)), function () {
+                    if ($(this).attr("service-id") == service_id) {
+                        var offset = $(this).offset();
+                        service_container.css("position", "absolute");
+                        service_container.addClass("anim");
+                        service_container.css("top", offset.top);
+                        service_container.css("left", offset.left);
+                        setTimeout(function () {
+                            service_container.css("opacity", "0");
+                            setTimeout(function () {
+                                $(service_container).remove();
+                            }, 300);
+                        }, 600);
+                        return false;
+                    }
+                });
+                return false;
+            }
+        });
+    });
+});
---
+++ libcis/DataAccessLogic/MarkAsViewedResult.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace libcis.DataAccessLogic
+{
+    class MarkAsViewedResult
+    {
+        public bool Success
+        {
+            get;
+            set;
+        }
+
+        public string ErrorMessage
+        {
+            get;
+            set;
+        }
+    }
+}
